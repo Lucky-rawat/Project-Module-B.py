9f59fa696016929b6359bcc555131f5e20179e76
RACHIT RAWAT <95635515+Lucky-rawat@users.noreply.github.com>
2022-06-28 11:24:46 +0530

    Create insertion_sort.py

---
+++ insertion_sort.py
@@ -0,0 +1,16 @@
+def InsertionSort(a):
+  
+    for i in range(1, len(a)):
+  
+        temp = a[i]
+
+        j = i-1
+        while j >=0 and temp < a[j] :
+                a[j+1] = a[j]
+                j -= 1
+        a[j+1] = temp
+
+a = [10, 5, 13, 8, 2]
+InsertionSort(a)
+print("Array after sorting:")
+print(a)
